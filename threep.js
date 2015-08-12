25d553d251f656eebe8f29a140531a53ddef094f
dann toliver <dann@bentobox.net>
2015-08-12 18:10:22 -0400

    Fixed element id issue; refactoring

--- threep.js
+++ threep.js
@@ -5,10 +5,13 @@ var NL = 30   // number of layers
 var BW = 300  // base width
 var BH = 300  // base height
 
-var data = [] // TODO: package point database seperately
+var VS = [ [0,0,0], [200,0,0], [100,200,0], [100,100,  NL-1  ] ]
+// var VS = [ [0,0,0], [200,0,0], [0,200,0], [300,200,0]]
+
+var db = [] // TODO: package point database seperately
 
 function add_point(x, y, z) {
-  data.push([x, y, z])
+  db.push([x, y, z])
   // TODO: add indices, etc
 }
 
@@ -17,11 +20,11 @@ function remove_point(x, y, z) {
 }
 
 function clear_points() {
-  data = []
+  db = []
 }
 
 function select_points(fun) {
-  return fun(data)
+  return fun(db)
   // TODO: make this more better
 }
 
@@ -42,14 +45,14 @@ function selector(lx, hx, ly, hy, lz, hz) {
     var acc = []
 
     for(var i = 0; i < data.length; i++) {
-      var d = data[i]
+      var d = data[i] // TODO: destructing
       var x = d[0]
       var y = d[1]
       var z = d[2]
 
-      if(lx !== null && (lx > x || hx <= x)) continue;
-      if(ly !== null && (ly > y || hy <= y)) continue;
-      if(lz !== null && (lz > z || hz <= z)) continue;
+      if(lx !== null && (lx > x || hx <= x)) continue
+      if(ly !== null && (ly > y || hy <= y)) continue
+      if(lz !== null && (lz > z || hz <= z)) continue
 
       acc.push(d)
     }
@@ -57,6 +60,7 @@ function selector(lx, hx, ly, hy, lz, hz) {
     return acc
   }
 }
+
 
 // RENDERER
 
@@ -96,6 +100,12 @@ function render_layer(n) {
   layer_els[n].textContent = str_for_layer(n)
 }
 
+function render_layers() {
+  for(var i = 0; i < NL; i++) {
+    render_layer(i)
+  }
+}
+
 function skew_layers(l, t) {
   for(var i = 0; i < NL; i++) {
     var layer = layer_els[i]
@@ -104,52 +114,24 @@ function skew_layers(l, t) {
   }
 }
 
+function init() {
+  make_layers(NL)
+}
+
 var layer_els = []
 var el = document.getElementById.bind(document)
 
-function init() {
+function make_layers(n) {
   var canvas = el('canvas')
   for(var i = 0; i < NL; i++) {
-    // TODO: make layer here
     var pre = document.createElement('pre')
-    pre.id = el('layer_'+(i+1))
+    pre.id = 'layer_'+(i+1)
     canvas.appendChild(pre)
     layer_els[i] = pre
-    // var layer = el('layer_'+(i+1))
-    // layer_els[i] = layer
-    // layer.style.top  = i + 'px'
-    // layer.style.left = i + 'px'
-    // layer.style.fontSize = (NL+8-i) + 'px'
   }
-
-  // z = 1
-
-  // for(x = 10; x < 30; x++) {
-  //   for(y = 15; y < 25; y++) {
-  //     // for(z = 0; z < 10; z++) {
-  //       add_point(x, y, z)
-  //       // add_point(x, y, 3)
-  //       // add_point(x, y+10, 2)
-  //     // }
-  //   }
-  // }
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  init()
-  sierp()
-  for(var i = 0; i < NL; i++) {
-    render_layer(i)
-  }
-
-  el('body').addEventListener('mousemove', function(e) {
-    skew_layers(e.clientX, e.clientY)
-  })
-})
-
-function sierp() {
-  // var vs = [ [0,0,0], [200,0,0], [100,200,0], [100,100,  NL-1  ] ]
-  var vs = [ [0,0,0], [200,0,0], [0,200,0], [300,200,0]]
+function chaos(vs) {
   var point = [100, 100, 0]
 
   for(var i = 0; i < 300000; i++) {
@@ -169,3 +151,51 @@ function sierp() {
     return list[Math.floor(Math.random() * list.length)]
   }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  init()
+
+  chaos(VS)
+
+  render_layers()
+
+
+  el('body').addEventListener('mousemove', function(e) {
+    onmove(e.clientX, e.clientY)
+  })
+})
+
+var mousemode = 'skew'
+
+function onmove(x, y) {
+  if(mousemode == 'skew') {
+    skew_layers(x, y)
+  } else if(mousemode == 'drag') {
+    console.log('drag')
+  }
+}
+
+
+
+// var layer = el('layer_'+(i+1))
+// layer_els[i] = layer
+// layer.style.top  = i + 'px'
+// layer.style.left = i + 'px'
+// layer.style.fontSize = (NL+8-i) + 'px'
+
+
+
+
+
+
+// z = 1
+
+// for(x = 10; x < 30; x++) {
+//   for(y = 15; y < 25; y++) {
+//     // for(z = 0; z < 10; z++) {
+//       add_point(x, y, z)
+//       // add_point(x, y, 3)
+//       // add_point(x, y+10, 2)
+//     // }
+//   }
+// }
